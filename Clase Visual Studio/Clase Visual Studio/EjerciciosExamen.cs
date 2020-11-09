c2a3c0bbaddd05b2e3d1d654f4529fcada5f7938
baeza20663 <71930502+baeza20663@users.noreply.github.com>
2020-11-09 20:40:11 +0100

    Ejercicios Nacho Cabanes
    Ejercicios for Nacho Cabanes más bandera japonesa deberes

--- Clase Visual Studio/Clase Visual Studio/EjerciciosExamen.cs
+++ Clase Visual Studio/Clase Visual Studio/EjerciciosExamen.cs
@@ -11,7 +11,7 @@ namespace Clase_Visual_Studio
         {
             return suma1 + suma2;
         }
-        public static double Reales2(double real1,double real2)
+        public static double Reales2(double real1, double real2)
         {
             return real1 - real2;
         }
@@ -19,17 +19,17 @@ namespace Clase_Visual_Studio
         {
             if (real1 < real2)
                 return real1;
-            else 
+            else
             if (real2 < real1)
                 return real2;
             else
                 return 0;
         }
-        public static string AddCadenas(string str1, string str2,string str3)
+        public static string AddCadenas(string str1, string str2, string str3)
         {
-           return str1 + str2 + str3;
+            return str1 + str2 + str3;
         }
-         public static int Compare2Integer(int number1, int number2)
+        public static int Compare2Integer(int number1, int number2)
         {
             if (number1 < number2)
                 return number2;
@@ -88,19 +88,19 @@ namespace Clase_Visual_Studio
         }
         public static void Add1(int number)
         {
-                        
-            for (int contador = 0; contador <= number ; contador++ )
+
+            for (int contador = 0; contador <= number; contador++)
             {
                 System.Console.Write(contador);
                 if (contador < number)
                 {
                     System.Console.Write(",");
-                    
+
                 }
                 else System.Console.Write(".");
 
             }
-                      
+
 
         }
         public static void AsteriskLine(int number)
@@ -108,7 +108,7 @@ namespace Clase_Visual_Studio
             for (int contador = 0; contador < number; contador++)
             {
                 System.Console.Write("*");
-                
+
             }
         }
         public static void AsteriskPatreon(int number)
@@ -174,16 +174,16 @@ namespace Clase_Visual_Studio
                 dividendo = cociente;
                 contador++;
             }
-            
+
             System.Console.WriteLine("");
             System.Console.WriteLine("El resultado es: " + "'" + resultado + "'");
             System.Console.WriteLine("Son " + contador + " dígitos");
 
-            
 
-            for (int contador3 = resultado.Length; contador3 > 0 ; contador3--)
+
+            for (int contador3 = resultado.Length; contador3 > 0; contador3--)
             {
-                
+
                 System.Console.Write(resultado.Substring(contador3 - 1, 1));
                 // contador3--;
             }
@@ -194,23 +194,81 @@ namespace Clase_Visual_Studio
             System.Console.Write("Introduce el número de columnas:");
             n = Convert.ToInt32(System.Console.ReadLine());
 
-            for (int f=1; f<=n; f++)
+            for (int f = 0; f < 10; f++)
             {
-                int espacios1 = n - f + 1;
-                int espacios2 = f * 2;
 
-                for (int c = 1; c <= f; c+=1)
+                for (int c = 0; c < 15; c += 1)
                 {
-                    for (int l = 1; l < espacios1; l++)
-                    {
+                    int suma = f + c;
+                    if ((suma % 2 == 0))
                         System.Console.Write("+");
-                    } // fin de la impresión de loquesea antes del asterisco
-                    System.Console.Write("*");
+
+                    else System.Console.Write(" ");
+
                 } // fin del contador de COLUMNAS c
-                               
+
                 System.Console.WriteLine();
             }  // fin del contador de FILAS f              
+
+        }
+
+        public static void Ejercicio2231()
+        {
+            int n;
+            for (n = 10; n < 21; n++)
+            {
+                System.Console.WriteLine("N vale:" + n);
+            }
             
+        }
+        public static void Ejercicio2232()
+        {
+            int n;
+            for (n = 1; n < 51; n = n+2)
+            {
+                System.Console.WriteLine("N vale:" + n);
+            }
+
+        }
+        public static void Ejercicio22313()
+        {
+            int n;
+            for (n = 100; n < 200; n++ )
+            {
+                if ((n%7) == 0 && (n%3) == 0)
+                    System.Console.WriteLine("N vale:" + n);
+            }
+
+        }
+        public static void Ejercicio2234()
+        {
+            int n;
+            for (n = 0; n < 11; n++)
+            {
+                
+                    System.Console.WriteLine("9x" + n + "=" + n*9);
+            }
+
+        }
+        public static void Ejercicio2235()
+        {
+            int n;
+            for (n = 1; n < 9; n++)
+            {
+
+                System.Console.WriteLine(n * 2);
+            }
+
+        }
+        public static void Ejercicio2236()
+        {
+            int n;
+            for (n = 15; n > 0; n--)
+            {
+
+                System.Console.WriteLine(n);
+            }
+
         }
 
     }
